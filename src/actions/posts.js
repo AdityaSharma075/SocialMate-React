a5e92de93b8875d08b907970bf121b883234383a
Aditya Sharma <aditya07543@gmail.com>
2021-11-19 15:46:27 +0530

    fetched post from api

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,8 +1,21 @@
+import { UPDATE_POST } from './actionTypes';
 export function fetchPosts() {
   return (dispatch) => {
-    const url = '/api/v1/posts?page=1&limit=5';
-    fetch(url).then((response) => {
-      console.log(response);
-    });
+    const url = 'api/v1/posts?page=1&limit=5';
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch(updatePost(data.posts));
+      });
+  };
+}
+
+export function updatePost(posts) {
+  return {
+    type: UPDATE_POST,
+    posts,
   };
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,3 +1,9 @@
-export default function posts(state = []) {
-  return state;
+import { UPDATE_POST } from '../actions/actionTypes';
+export default function posts(state = [], action) {
+  switch (action.type) {
+    case UPDATE_POST:
+      return action.posts;
+    default:
+      return state;
+  }
 }
