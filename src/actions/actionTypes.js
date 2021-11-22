192245c695be438257b6f3dadf6176223e787c4d
Aditya Sharma <aditya07543@gmail.com>
2021-11-22 13:59:24 +0530

    Add user log-in auth reducers and action type

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,6 @@
 export const UPDATE_POST = 'UPDATE_POST';
+
+// login actiontypes
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
---
+++ src/reducers/auth.js
@@ -0,0 +1,36 @@
+import {
+  LOGIN_FAILED,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from '../actions/actionTypes';
+const initialAuthState = {
+  user: {},
+  error: null,
+  isLoggedin: false,
+  inProgress: false,
+};
+export default function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        isLoggedin: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedin: true,
+        inProgress: false,
+        error: null,
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
